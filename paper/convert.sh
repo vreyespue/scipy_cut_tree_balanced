0be53511e6793803c0af2445289638b3cec6a4fd
ljmartin <lewis.martin44@gmail.com>
2020-10-17 21:55:29 +1100

    intro para

--- paper/convert.sh
+++ paper/convert.sh
@@ -1,3 +1,4 @@
 pandoc paper.md -C -o paper.pdf --bibliography references.bib --filter pandoc-secnos --filter pandoc-fignos
+#pandoc paper.md -C -o paper.pdf --bibliography references.bib --filter pandoc-secnos --filter pandoc-crossref --filter pandoc-fignos
 #pandoc paper.md -C -o paper.pdf --bibliography references.bib --filter pandoc-crossref
 
