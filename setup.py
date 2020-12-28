ac194c80d1ffb51f93257bd33209ac1d82f2d591
Vicente Reyes-Puerta <vr.github@outlook.com>
2020-12-29 00:34:53 +0100

    Fixes in setup.py; add setup.cfg

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scipy_cut_tree_balanced",
-    version="1.0.1",
+    version="0.1",
     author="Vicente Reyes-Puerta",
-    author_email="vr.github@hotmail.com",
+    author_email="vr.github@outlook.com",
     description="Python function that performs a balanced cut tree of a SciPy linkage matrix",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/vreyespue/scipy_cut_tree_balanced",
     packages=setuptools.find_packages(),
+    install_requires=["scipy", "numpy", "pytest"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
